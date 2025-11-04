a51a33333efeed675aba7caa9d77eae8568e768d
murphy-afk <bardarenoemi@gmail.com>
2025-11-04 14:29:53 +0100

    feat: iniziali

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -1,13 +1,23 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/* Scrivi una funzione che accetti un array di nomi
+  e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito
+*/
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+const getInitials = (array) => {
+  const initials = [];
+  for (let i = 0; i < array.length; i++) {
+    const curName = array[i];
+    initials.push(curName[0]);
+  }
+  return initials
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(getInitials(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]

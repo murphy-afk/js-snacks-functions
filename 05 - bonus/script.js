cffb935dadc82c73831e09964e090c507e269a5c
murphy-afk <bardarenoemi@gmail.com>
2025-11-04 14:58:08 +0100

    feat: bonus

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -1,4 +1,5 @@
-/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito. 
+/* Scrivi una funzione che accetti una stringa contenente un nome 
+e restituisca un saluto seguito dal nome fornito. 
 Il saluto deve essere :
 buongiorno se è mattina (fino alle 13), 
 buon pomeriggio se è pomeriggio (fino alle 17) 
@@ -10,9 +11,21 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
+const getGreetingType = () => {
+  let greeting;
+  const now = new Date();
+  const hour = now.getHours();
+  if (hour < 13) greeting = "Buongiorno";
+  if (hour > 13 && hour < 17) greeting = "Buon pomeriggio";
+  if (hour > 17) greeting = "Buonasera";
+  return greeting
+}
+
+const printGreeting = (name) => getGreetingType() + " " + name;
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(printGreeting(name));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.

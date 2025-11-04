93c87752747da6ca54cf900622cd8a8205dce6c7
murphy-afk <bardarenoemi@gmail.com>
2025-11-04 14:37:52 +0100

    feat: filtra iniziali

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -1,13 +1,24 @@
-/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
+/* Scrivi una funzione che accetti un'array di stringhe e una lettera
+ che restituisca un array contenente solo le parole che iniziano con quella lettera 
+*/
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
-// Dichiara la funzione qui.
-
+const filterByInitial = (array, letter) => {
+  const filteredArray = [];
+  for (let i = 0; i < array.length; i++) {
+    const curName = array[i];
+    if (curName[0] === letter) {
+      filteredArray.push(curName)
+    }
+  }
+  return filteredArray
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(filterByInitial(names, "A"));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]

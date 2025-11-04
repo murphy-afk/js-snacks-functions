ee749bd7ae1d4b880b7d893009a4dcc94c9c8356
murphy-afk <bardarenoemi@gmail.com>
2025-11-04 17:43:48 +0100

    feat: (alfabeto farfallino) solution by case

---
+++ 06- alfabeto farfallino/script.js
@@ -0,0 +1,30 @@
+// Esercizio 1 (Alfabeto Farfallino)
+
+// Scrivi un programma in JavaScript che chieda all’utente di inserire una parola
+// o una frase e ne restituisca la versione tradotta secondo la regola dell’alfabeto farfallino.
+
+// In questo linguaggio giocoso, ogni vocale della parola viene ripetuta
+// e tra le due copie viene inserita la lettera "f".
+
+// Esempio:
+// Input:  ciao
+// Output: cifaifao
+// non dovrebbe essere: cifiafaofo ?
+
+// scorri parola 
+// trova le vocali 
+// per ogni vocale 
+// fai vocale + f + vocale
+// restituisci una parola nuova dove le consonanti sono invariate e le vocali sostitute
+
+const wordExample = "ciao";
+const vowels = "aeiou";
+let translatedWord = "";
+
+for (let i = 0; i < wordExample.length; i++) {
+  const letter = wordExample[i];
+  const letterIsAVowel = vowels.includes(letter);
+  if (letterIsAVowel) translatedWord += letter + "f";
+  translatedWord += letter;
+}
+console.log(translatedWord);

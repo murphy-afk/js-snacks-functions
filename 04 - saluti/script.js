2ca213659d1a781091585d8ae8969fe421c46edb
murphy-afk <bardarenoemi@gmail.com>
2025-11-04 14:47:38 +0100

    feat: saluti

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,17 @@
-/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+/* Scrivi una funzione che accetti una stringa contenente un nome
+ e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") 
+ */
 
 const name = 'Mario';
 
 
 // Dichiara la funzione qui.
 
+const sayHello = (name) => `Ciao ${name}`;
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(sayHello(name));
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario

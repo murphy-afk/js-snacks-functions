62db7834acf6d13a797b847f47c3208b613850cb
murphy-afk <bardarenoemi@gmail.com>
2025-11-04 14:45:37 +0100

    feat: conta vocali

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,26 @@
-/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
+/* Scrivi una funzione che accetti una stringa
+ e restituisca il numero di vocali contenute al suo interno 
+ */
 
 const word = 'javascript';
 
 
 // Dichiara la funzione qui.
 
+const countVowels = (word) => {
+  let result = 0;
+  for (let i = 0; i < word.length; i++) {
+    let curLetter = word[i];
+    if (curLetter === "a" || curLetter === "e" || curLetter === "i" || curLetter === "o" || curLetter === "u") {
+      result++;
+    }
+  }
+  return result
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(countVowels(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)

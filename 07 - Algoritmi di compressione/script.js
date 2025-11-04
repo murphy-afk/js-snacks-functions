c8bc8d3fc04419d69d1fcd3250d7e6c8f24f5f86
murphy-afk <bardarenoemi@gmail.com>
2025-11-04 18:25:12 +0100

    feat: (algoritml di compressione) solution by case

---
+++ 07 - Algoritmi di compressione/script.js
@@ -0,0 +1,32 @@
+// Scrivi una funzione JavaScript chiamata myCompress 
+// che riceve come parametro una stringa e restituisce una nuova stringa 
+// in cui le sequenze di caratteri ripetuti consecutivamente vengono compresse.
+
+// Ogni gruppo di caratteri uguali deve essere rappresentato dal carattere stesso 
+// seguito dal numero di ripetizioni racchiuso tra parentesi angolari (< >), 
+// solo se la ripetizione è maggiore di 1.
+
+// La funzione deve restituire la stringa compressa risultante.
+
+// Esempio di comportamento atteso:
+// myCompress('Haapppy Codiiiiinnnng')
+// // Output: "Ha<2>p<3>y Codi<5>n<4>g"
+
+const wordExample = "Ciaaao a tttuutti";
+
+
+
+let countDuplicates = 1;
+let formattedWord = "";
+for (let i = 0; i < wordExample.length; i++) {
+  const letter = wordExample[i];
+  if (letter === wordExample[i + 1]) countDuplicates++;
+  else {
+    formattedWord += letter;
+    if (countDuplicates > 1) formattedWord += `<${countDuplicates}>`;
+  }
+}
+
+
+
+console.log(formattedWord);
